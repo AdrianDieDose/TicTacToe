05727d87e2b1c92802fb1bb548fcb1c72cb82007
AdrinDieDose <adrinator1@gmail.com>
2020-06-26 17:15:37 +0200

    Added all functionality

--- script.js
+++ script.js
@@ -1,2 +1,96 @@
-const statusDisplay = document.getElementById('game-container');
+const statusDisplay = document.getElementById('game-status');
 
+let gameActive = true;
+
+let currentPlayer = "X";
+
+let gameState =    ["", "", "",
+                    "", "", "", 
+                    "", "", "",];
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const winningMassage = () => `Player ${currentPlayer} has WON !`;
+const drawMassage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `Its ${currentPlayer}'s turn!`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+gameState[clickedCellIndex] = currentPlayer;
+clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    if(currentPlayer === "X") {
+        currentPlayer = "O";
+    } else {
+        currentPlayer = "X";
+    }
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if(a === '' || b === '' || c === '') {
+            continue;
+        }
+        if(a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+    if(roundWon) {
+        statusDisplay.innerHTML = winningMassage();
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if(roundDraw) {
+        statusDisplay.innerHTML = drawMassage();
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+
+
+    if (!gameActive || gameState[clickedCellIndex] !== "") {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState =    ["", "", "","", "", "", "", "", "",];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.restart-game').addEventListener('click', handleRestartGame);
